4fcffa972e23852d67aca82102f6693495c240d3
ADMIN <nguyenduchieu1812@gmail.com>
2025-04-20 18:25:23 +0700

    feat: Thêm thông tin địa chỉ nhận

--- src/app.tsx
+++ src/app.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import { Route } from "react-router-dom";
 import ProductDetail from "./pages/product-detail";
 
-<Route path="/product/:id" element={<ProductDetail />} /> 
+<Route path="/product/:id" element={<ProductDetail />} />;
---
+++ src/types/address.ts
@@ -0,0 +1,23 @@
+export interface AddressData {
+  key_id: string;
+  id: string;
+  parent_id: string;
+  key_parent_id: string;
+  name: string;
+  level: number;
+}
+
+export enum AddressLevel {
+  Province = 1, // Tỉnh/Thành phố
+  District = 2, // Quận/Huyện
+  Ward = 3, // Phường/Xã
+  Hamlet = 4, // Ấp/Thôn
+}
+
+export interface ShippingAddress {
+  id: string;
+  fullName: string;
+  phone: string;
+  address: string;
+  isDefault?: boolean;
+}
